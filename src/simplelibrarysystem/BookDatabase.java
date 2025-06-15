6b3e62d9db1c05223d4294160a600f480d0661ba
Donut <Donut@192.168.1.106>
2025-06-15 21:53:01 +1200

    finished most of the code for the methods: - addBook - getAllBooks 	dont know why myQuery isnt working all try different fix

--- src/simplelibrarysystem/BookDatabase.java
+++ src/simplelibrarysystem/BookDatabase.java
@@ -4,9 +4,13 @@
  */
 package simplelibrarysystem;
 
-import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 /**
  *
@@ -20,13 +24,36 @@ public class BookDatabase {
         this.conn = DBManager.getInstance().getConnection();
     }
     
-    public void addBook(){
-        
+    public void addBook(Book book){
+        String sql = "INSERT INTO BOOKS (TITLE, AUTHOR, BARCODE) VALUES(?, ?, ?)";
+        try{
+            PreparedStatement infoIntoSqlStatement = conn.prepareStatement(sql);
+            infoIntoSqlStatement.setString(1, book.getTitle());
+            infoIntoSqlStatement.setString(2, book.getAuthor());
+            infoIntoSqlStatement.setString(3, book.getBarcode());
+            infoIntoSqlStatement.executeUpdate();
+        } catch (SQLException ex){
+            System.out.println("Error with:" + ex.getMessage());
+        }
     }
     
     public List<Book> getAllBooks(){
-        List<Book> s = new ArrayList<>();
-        return s;
+        List<Book> books = new ArrayList<>();
+        try {
+            Statement statment = conn.createStatement();
+            ResultSet rs = conn.myQuery("select * from BOOKS");
+            while(rs.next()){
+                String title = rs.getString("TITLE");
+                String author = rs.getString("AUTHOR");
+                String barcode = rs.getString("BARCODE");
+                
+                Book book = new Book(title, author, barcode);
+                books.add(book);
+            }
+        } catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        return books;
     }
     
     public void updateBook(){
